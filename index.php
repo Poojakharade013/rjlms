ad15c0fbfd1d6578a57ca6d9efb3c05246450d1b
Poojakharade013 <142389286+Poojakharade013@users.noreply.github.com>
2024-03-03 20:57:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'connection.php';
+include 'topnav.php';
+?>
+
+          <!-- Breadcrumbs-->
+           
+          </ol>
+           <div class="Student_logo">
+            <a href="#" class="image full"><img src="https://mltnu5r4mmy0.i.optimole.com/cb:HrXz.42586/w:auto/h:auto/q:mauto/f:avif/id:5c2e4118bbb252b1f9eb4ebf8769952b/https://rebuiltech.com/jhunjhunwala-college-01.jpg" style="width:1268px;" style="background-attachment: fixed;" style="background-size: contain;"  >RJ COLLEGE'S LMS</a>
+          <!-- Page Content -->
+          <h4></h4>
+          <hr> 
+          <p> <h5>  </h5></p>
+<?php include 'footer.php'; ?>
